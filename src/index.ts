f0e7e6f1d59bcad69a49d234ba4e9bba20df8e58
plirapli <rafliyulistiawan@gmail.com>
2025-06-08 20:31:21 +0700

    rm prisma

--- src/index.ts
+++ src/index.ts
@@ -6,10 +6,11 @@ import * as phone from "./controllers/phone.controller";
 const app = new Hono<{ Bindings: Bindings }>().basePath("/api/v1");
 app.use("/api/*", cors());
 
-app.get("/phones", phone.get);
-app.get("/phones/:id", phone.getById);
-app.post("/phones", phone.create);
-app.put("/phones/:id", phone.updateById);
-app.delete("/phones/:id", phone.deleteById);
+app
+  .get("/phones", phone.get)
+  .get("/phones/:id", phone.getById)
+  .post("/phones", phone.create)
+  .put("/phones/:id", phone.updateById)
+  .delete("/phones/:id", phone.deleteById);
 
 export default app;
